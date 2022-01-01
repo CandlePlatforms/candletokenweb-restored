9d7c3b3a1a12b6280f0a3580a0d6cbcf33c6367c
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-01-01 00:31:43 -0800

    Update index.tsx

--- components/sections/home/hero/index.tsx
+++ components/sections/home/hero/index.tsx
@@ -14,21 +14,6 @@ const HomeHero = ({ title, subtitle, cta, videoLabel }) => {
   const headerRef = useRef<HTMLDivElement>(null);
   const dotsRef = useRef<HTMLDivElement>(null);
 
-  const videos = [
-    "/hero-videos/1.mp4",
-    "/hero-videos/2.mp4",
-    "/hero-videos/3.mp4",
-    "/hero-videos/4.mp4",
-  ];
-
-  const onVideoChange = () => {
-    if (globeDotIndex < 3) {
-      setGlobeDotIndex(globeDotIndex + 1);
-    } else {
-      setGlobeDotIndex(0);
-    }
-  };
-
   useEffect(() => {
     if (!dotsRef.current || !videoBoxRef.current) return;
 
@@ -164,7 +149,7 @@ const HomeHero = ({ title, subtitle, cta, videoLabel }) => {
           <div ref={dotsRef}>
             <GlobeDot
               pulsating={globeDotIndex === 0}
-              image="/images/hero/avatar-1.png"
+              image="/images/hero/avatar-candle.png"
               left={["10%", "35%"]}
               top={["5%", "15%"]}
             />
@@ -182,20 +167,20 @@ const HomeHero = ({ title, subtitle, cta, videoLabel }) => {
             />
             <GlobeDot
               pulsating={globeDotIndex === 1}
-              image="/images/hero/avatar-2.png"
+              image="/images/hero/avatar-candle.png"
               left={["35%", "32%"]}
               top={["40%", "50%"]}
             />
 
             <GlobeDot
               pulsating={globeDotIndex === 2}
-              image="/images/hero/avatar-3.png"
+              image="/images/hero/avatar-candle.png"
               left={["70%", "67%"]}
               top={["35%", "62%"]}
             />
             <GlobeDot
               pulsating={globeDotIndex === 3}
-              image="/images/hero/avatar-4.png"
+              image="/images/hero/avatar-candle.png"
               left={["50%", "55%"]}
               top={["15%", "20%"]}
             />
@@ -212,24 +197,7 @@ const HomeHero = ({ title, subtitle, cta, videoLabel }) => {
               top={["75%", "70%"]}
             />
           </div>
-          <div
-            ref={videoBoxRef}
-            sx={{
-              position: "absolute",
-              left: "50%",
-              bottom: "-2%",
-              transform: "translateX(-50%)",
-              zIndex: "100",
-              borderRadius: "6px",
-              overflow: "hidden",
-              opacity: 0,
-            }}>
-            <VideoSwapper
-              videoLabel={videoLabel}
-              sources={videos}
-              onChange={onVideoChange}
-            />
-          </div>
+
         </Box>
       </Container>
     </Box>
